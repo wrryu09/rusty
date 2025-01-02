233421dc9ab5430d5b447da3c089bf4ac97d7b31
wrryu09 <rryu09@ewhain.net>
2025-01-03 00:24:06 +0900

    feat: add testobject

--- src/pages/main.rs
+++ src/pages/main.rs
@@ -1,13 +1,40 @@
 use yew::prelude::*;
 
-pub struct MainPage;
+struct TestObject{
+    id: i32,
+    name: String,
+    desc: String
+}
+
+struct State{
+    items: Vec<TestObject>
+}
+
+pub struct MainPage{
+    state: State,
+}
 
 impl Component for MainPage {
     type Message = ();
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
-        Self
+        let items: Vec<TestObject> = vec![
+            TestObject{
+                id: 0,
+                name: "firstObj".to_string(),
+                desc: "first obj in mainpage".to_string(),
+            },
+            TestObject{
+                id: 1,
+                name: "scndObj".to_string(),
+                desc: "second".to_string(),
+            },
+        ];
+
+        Self{
+            state: State { items, }
+        }
     }
 
     fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
@@ -19,6 +46,15 @@ impl Component for MainPage {
     }
 
     fn view(&self, _ctx: &Context<Self>) -> Html {
-        html! { <span>{"Main page"}</span> }
+        let items: Vec<Html> = self.state.items.iter().map(|obj:&TestObject| {
+            html!{
+                <div>
+                    <p>{&obj.id}</p>
+                    <p>{&obj.name}</p>
+                    <p>{&obj.desc}</p>
+                </div>
+            }
+        }).collect();
+        html! { <span>{items}</span> }
     }
 }
